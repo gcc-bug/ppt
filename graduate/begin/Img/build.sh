d7b8f79d01cd02a65c7dde41c47087937dc40cc0
gcc_bug <by.gdc@outlook.com>
2023-12-12 14:38:33 +0800

    modify path

--- graduate/begin/Img/build.sh
+++ graduate/begin/Img/build.sh
@@ -1,9 +1,22 @@
 
 #!/bin/bash
 
-for file in *.tex
+for file in ../../Image/*.tex
 do
   pdflatex "$file"
 done
-
 echo "Compilation of all LaTeX files complete."
+
+for file in ../../Image/*.png
+do
+  convert "$file" "${file%.png}.pdf"
+  cp "${file%.png}.pdf" .
+done
+echo "Convert of all png"
+
+for file in ../../Image/*.svg
+do
+  echo "${file}"
+  cp "$file" .
+done
+echo "copy all svg"
