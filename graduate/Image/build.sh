fc94266cbb502f202230bd537ce42e80ac7a357f
gcc_bug <by.gdc@outlook.com>
2023-12-19 21:35:12 +0800

    modif something

--- graduate/Image/build.sh
+++ graduate/Image/build.sh
@@ -14,4 +14,10 @@ do
   convert "$file" "${file%.png}.pdf"
 done
 
+for file in *.svg
+do
+  echo "${file%.svg}.pdf"
+  convert "$file" "${file%.svg}.pdf"
+done
+
 echo "Convertion of all png complete."
